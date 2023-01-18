04b007c81b0b2e75bd32595c5013a17489633000
seonjakim <seonja.kim0@gmail.com>
2023-01-18 17:53:14 +0900

    refactor: extract function and put it inline

--- index.js
+++ index.js
@@ -8,19 +8,22 @@ function statement(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
     const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID]
-        let thisAmount = amountFor(perf, play)
+        let thisAmount = amountFor(perf, playFor(perf))
 
         volumeCredits += Math.max(perf.audience - 30, 0)
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5)
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
 
 
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
+        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
         totalAmount += thisAmount
     }
     result += `총액 ${format(totalAmount / 100)}\n`
     result += `적립 포인트: ${volumeCredits}점\n`
     return result
+}
+
+function playFor(aPerformance) {
+    return plays[aPerformance.playID]
 }
 
 function amountFor(aPerformance, play) {
@@ -37,7 +40,7 @@ function amountFor(aPerformance, play) {
             if (aPerformance.audience > 20) {
                 result += 10000 + 500 * (aPerformance.audience - 20)
             }
-            result += 300 * perf.audience
+            result += 300 * aPerformance.audience
         break;
         default:
             throw new Error(`알 수 없는 장르: ${play.type}`)
