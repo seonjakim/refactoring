70743019d05780e8401b28ed6b93acc9d9da8a71
seonjakim <seonja.kim0@gmail.com>
2023-01-18 17:58:33 +0900

    변수 인라인

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ function statement(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
     const format = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD", minimumFractionDigits: 2}).format
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf))
-
         volumeCredits += Math.max(perf.audience - 30, 0)
         if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5)
 
 
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
-        totalAmount += thisAmount
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf, playFor(perf)) / 100)} (${perf.audience}석)\n`
+        totalAmount += amountFor(perf, playFor(perf))
     }
     result += `총액 ${format(totalAmount / 100)}\n`
     result += `적립 포인트: ${volumeCredits}점\n`
