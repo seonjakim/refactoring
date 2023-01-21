caa95b0762589171a45136662a217262eee79c57
seonjakim <seonja.kim0@gmail.com>
2023-01-21 14:07:26 +0900

    함수 내부 변수 rename

--- index.js
+++ index.js
@@ -48,19 +48,19 @@ function volumeCreditsFor(aPerformance) {
 }
 
 function totalVolumeCredits() {
-    let volumeCredits = 0
+    let result = 0
     for (let perf of invoices.performances) {
-        volumeCredits += volumeCreditsFor(perf)
+        result += volumeCreditsFor(perf)
     }
-    return volumeCredits
+    return result
 }
 
 function totalAmount() {
-    let totalAmount = 0
+    let result = 0
     for (let perf of invoices.performances) {
-        totalAmount += amountFor(perf)
+        result += amountFor(perf)
     }
-    return totalAmount
+    return result
 }
 
 function usd(aNumber) {
