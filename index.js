e005d6dbc3fa606a415e9ac59ff830dc760a3602
seonjakim <seonja.kim0@gmail.com>
2023-01-21 14:05:46 +0900

    totalAmount 랜덤 함수 이름 붙여서 리팩터

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const plays = JSON.parse(await readFile("./plays.json"))
 const invoices = JSON.parse(await readFile("./invoices.json"))
 
 function statement(invoice, plays) {
-    let totalAmount = 0
     let result = `청구 내역 (고객명: ${invoice.customer})\n`
     for (let perf of invoice.performances) {       
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf, playFor(perf)))} (${perf.audience}석)\n`
-        totalAmount += amountFor(perf, playFor(perf))
     }
+    let totalAmount = appleSauce()
     result += `총액 ${usd(totalAmount)}\n`
     result += `적립 포인트: ${totalVolumeCredits()}점\n`
     return result
@@ -55,6 +54,14 @@ function totalVolumeCredits() {
         volumeCredits += volumeCreditsFor(perf)
     }
     return volumeCredits
+}
+
+function appleSauce() {
+    let totalAmount = 0
+    for (let perf of invoices.performances) {
+        totalAmount += amountFor(perf)
+    }
+    return totalAmount
 }
 
 function usd(aNumber) {
