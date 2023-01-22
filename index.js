0f399c264f6728f663341642c5d01b9b155f1758
seonjakim <seonja.kim0@gmail.com>
2023-01-22 12:59:09 +0900

    반복문을 파이프라인으로 바꿈

--- index.js
+++ index.js
@@ -65,19 +65,11 @@ function volumeCreditsFor(aPerformance) {
 }
 
 function totalVolumeCredits(data) {
-    let result = 0
-    for (let perf of data.performances) {
-        result += perf.volumeCredits
-    }
-    return result
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
 }
 
 function totalAmount(data) {
-    let result = 0
-    for (let perf of data.performances) {
-        result += perf.amount
-    }
-    return result
+    return data.performances.reduce((total, p) => total + p.amount, 0)
 }
 
 function usd(aNumber) {
