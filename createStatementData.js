ffbea90e5969e318c84031ba2905642fcb262ce1
seonjakim <seonja.kim0@gmail.com>
2023-01-30 09:17:09 +0900

    move amountFor and volumeCreditsFor function inside PerformanceCalculator class

--- createStatementData.js
+++ createStatementData.js
@@ -8,25 +8,34 @@ class PerformanceCalculator {
     }
 
     get amount() {
-    let result = 0
-    switch (this.play.type) {
-        case "tragedy":
-            result = 4000
-            if (this.performance.audience > 30) {
-                result += 1000 * (this.performance.audience - 30)
-            }
-        break;
-        case "comedy":
-            result = 30000
-            if (this.performance.audience > 20) {
-                result += 10000 + 500 * (this.performance.audience - 20)
-            }
-            result += 300 * this.performance.audience
-        break;
-        default:
-            throw new Error(`알 수 없는 장르: ${this.play.type}`)
+        let result = 0
+        switch (this.play.type) {
+            case "tragedy":
+                result = 4000
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30)
+                }
+            break;
+            case "comedy":
+                result = 30000
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20)
+                }
+                result += 300 * this.performance.audience
+            break;
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`)
+        }
+        return result
     }
-    return result
+
+    get volumeCredits() {
+        let result = 0
+        result += Math.max(this.performance.audience - 30, 0)
+        if ('comedy' === this.play.type) {
+            result += Math.floor(this.performance.audience / 5)
+        }
+        return result
     }
 }
 
@@ -43,8 +52,8 @@ function enrichPerformance(aPerformance) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance)
     result.play = playFor(result)
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
 }
 function playFor(aPerformance) {
@@ -56,12 +65,6 @@ function amountFor(aPerformance) {
 }
 
 function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === aPerformance.play.type) {
-        result += Math.floor(aPerformance.audience / 5)
-    }
-    return result
 }
 
 function totalVolumeCredits(data) {
